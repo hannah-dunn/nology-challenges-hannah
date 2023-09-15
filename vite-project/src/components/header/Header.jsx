fc7ba1d56e4f3c4272876f84dbfb0b7ecf437809
Hannah <h.dunn@spin.net.au>
2023-09-15 12:38:09 +1000

    adds react and vite intro

---
+++ vite-project/src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import styles from "./Header.module.scss";
+
+const Header = () => {
+  console.log(styles);
+
+  return (
+    <header>
+      <h1>My first Component</h1>
+      <p>Learning React</p>
+    </header>
+  );
+};
+
+export default Header;
